fbd151a9e325b443aed8f516d7085c59bbdcbfbc
Carlos Rogério Campos Anselmo <rogerio.anselmo@lambda3.com.br>
2020-06-14 15:33:48 -0300

    swagger

---
+++ src/Nibo.Api/API/Extensions/Swagger.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class Swagger
+    {
+        public static void AddSwagger(this IServiceCollection services) =>
+            services.AddSwaggerGen(x => x.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "Nibo Api",
+                    Version = "1.0"
+                }));
+
+        public static void AddSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nibo"));
+
+        }
+    }
+}
